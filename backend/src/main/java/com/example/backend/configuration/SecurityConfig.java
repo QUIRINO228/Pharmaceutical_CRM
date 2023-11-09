793724903d7604ca6c19e718662ce508db73d769
QUIRINO228 <114148654+QUIRINO228@users.noreply.github.com>
2023-11-09 16:37:50 +0200

    fixed bug

--- backend/src/main/java/com/example/backend/configuration/SecurityConfig.java
+++ backend/src/main/java/com/example/backend/configuration/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -42,6 +43,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers("/registration", "/", "/resources/**", "/products/*/bid", "/product/**", "/images/**", "/user/**").permitAll()
                         .anyRequest().authenticated()
