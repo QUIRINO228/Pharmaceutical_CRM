1db9d2161fa4bf95f9c16a4fe38034eb310ef97d
QUIRINO228 <114148654+QUIRINO228@users.noreply.github.com>
2023-11-11 23:11:19 +0200

    commit

--- backend/src/main/java/com/example/backend/models/Product.java
+++ backend/src/main/java/com/example/backend/models/Product.java
@@ -1,10 +1,17 @@
 package com.example.backend.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "products")
-
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
 
     @Id
@@ -17,67 +24,4 @@ public class Product {
     private String availability_quantity;
     private String supplier;
     private String expiration_date;
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhoto() {
-        return photo;
-    }
-
-    public void setPhoto(String photo) {
-        this.photo = photo;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getPrice() {
-        return price;
-    }
-
-    public void setPrice(String price) {
-        this.price = price;
-    }
-
-    public String getAvailability_quantity() {
-        return availability_quantity;
-    }
-
-    public void setAvailability_quantity(String availability_quantity) {
-        this.availability_quantity = availability_quantity;
-    }
-
-    public String getSupplier() {
-        return supplier;
-    }
-
-    public void setSupplier(String supplier) {
-        this.supplier = supplier;
-    }
-
-    public String getExpiration_date() {
-        return expiration_date;
-    }
-
-    public void setExpiration_date(String expiration_date) {
-        this.expiration_date = expiration_date;
-    }
 }
