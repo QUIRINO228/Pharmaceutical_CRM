aab5b5539cc936a61e274eb8fde8ce3d3dbc491e
QUIRINO228 <114148654+QUIRINO228@users.noreply.github.com>
2023-11-11 22:49:22 +0200

    Update registration.component.ts

--- frontend/src/app/components/registration/registration.component.ts
+++ frontend/src/app/components/registration/registration.component.ts
@@ -6,7 +6,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['registration.component.css']
+  styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent {
   registrationForm: FormGroup;
