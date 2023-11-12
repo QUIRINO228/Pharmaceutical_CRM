92d0e789811d1f824df24a0d871ffdcf58080e08
anchous <orlovska.anna@lll.kpi.ua>
2023-11-12 13:39:58 +0200

    fixed bug

---
+++ frontend/src/app/Product.ts
@@ -0,0 +1,10 @@
+export interface Product{
+    id: number
+    name: string
+    photo: string
+    description: string
+    price: number
+    availability_quantity: number
+    supplier: string
+    expiration_date: number
+}
--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,12 +3,18 @@ import { RouterModule, Routes } from "@angular/router";
 import { RegistrationComponent } from "./components/registration/registration.component";
 import { LoginComponent } from "./components/login/login.component";
 import {ProductComponent} from "./components/product/product.component";
+import { AddproductComponent } from './components/addproduct/addproduct.component';
+import { UpdateproductComponent } from './components/updateproduct/updateproduct.component';
+import { ViewproductsComponent } from './components/viewproduct/viewproduct.component';
 
 const routes: Routes = [
   { path: 'registration', component: RegistrationComponent },
   { path: 'login', component: LoginComponent },
   { path: 'product', component: ProductComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', component: ViewproductsComponent },
+  { path: 'add', component: AddproductComponent },
+  { path: 'update/:id', component: UpdateproductComponent }
 ];
 
 @NgModule({
