26f3b75ae488b4e6d28118058180b50053ef6220
QUIRINO228 <114148654+QUIRINO228@users.noreply.github.com>
2023-11-28 13:19:05 +0200

    commit

---
+++ frontend/src/app/services/guards/admin-gurad/admin.guard.ts
@@ -0,0 +1,43 @@
+import {ActivatedRouteSnapshot, CanActivate, CanActivateFn, Router, RouterStateSnapshot} from '@angular/router';
+import {Injectable} from "@angular/core";
+import {StorageService} from "../../storage/storage.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
+
+
+
+@Injectable({
+  providedIn: "root"
+})
+
+export class AdminGuard implements CanActivate {
+
+  constructor(private router: Router,
+              private snackbar: MatSnackBar) {
+  }
+
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    if (StorageService.isWorkerLoggedIn()){
+      this.router.navigateByUrl("/worker/dashboard");
+      this.snackbar.open("You dont have access to this page", "Close",{duration: 5000})
+      return false;
+    }
+    if (StorageService.isManagerLoggedIn()){
+      this.router.navigateByUrl("/manager/dashboard");
+      this.snackbar.open("You dont have access to this page", "Close",{duration: 5000})
+      return false;
+    }
+    if (StorageService.isUserLoggedIn()){
+      this.router.navigateByUrl("/user/dashboard");
+      this.snackbar.open("You dont have access to this page", "Close",{duration: 5000})
+      return false;
+    }
+    if(!StorageService.hasToken()){
+      StorageService.logout()
+      this.router.navigateByUrl("/login");
+      this.snackbar.open("You are not loggedIn", "Close",{duration: 5000})
+      return false;
+    }
+    return true;
+  }
+
+}
---
+++ frontend/src/app/services/guards/noAuth-guard/no-auth.guard.ts
@@ -0,0 +1,36 @@
+import {ActivatedRouteSnapshot, CanActivate, CanActivateFn, Router, RouterStateSnapshot} from '@angular/router';
+import {Injectable} from "@angular/core";
+import {StorageService} from "../../storage/storage.service";
+
+
+@Injectable({
+  providedIn: "root"
+})
+
+export class noAuthGuard implements CanActivate {
+
+  constructor(private router: Router) {
+  }
+
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    if (StorageService.hasToken() && StorageService.isUserLoggedIn()) {
+      this.router.navigateByUrl("/worker/dashboard");
+      return false;
+    }
+    if (StorageService.hasToken() && StorageService.isAdminLoggedIn()) {
+      this.router.navigateByUrl("/admin/dashboard");
+      return false;
+    }
+    if (StorageService.hasToken() && StorageService.isWorkerLoggedIn()) {
+      this.router.navigateByUrl("/worker/dashboard");
+      return false;
+    }
+    if (StorageService.hasToken() && StorageService.isManagerLoggedIn()) {
+      this.router.navigateByUrl("/manager/dashboard");
+      return false;
+    }
+    return true;
+  }
+
+}
+
