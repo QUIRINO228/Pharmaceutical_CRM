e9209616de552ee54e7bae3697121e868692a8b2
QUIRINO228 <114148654+QUIRINO228@users.noreply.github.com>
2023-11-30 12:34:15 +0200

    Update app.service.ts

--- frontend/src/app/app.service.ts
+++ frontend/src/app/app.service.ts
@@ -7,7 +7,6 @@ import {ForgotCodeDTO} from './ForgotCodeDTO'
 import {MatDialog} from '@angular/material/dialog';
 import {RegistrationComponent} from "./components/registration/registration.component";
 import {AddproductComponent} from "./components/addproduct/addproduct.component";
-import {ForgotMessageComponent} from "./components/forgot-message/forgot-message.component";
 
 
 @Injectable({
@@ -88,12 +87,6 @@ export class AppService {
 
   openRegistrationDialog(): void {
     this.dialog.open(RegistrationComponent, {
-    });
-  }
-  openForgotPasswordDialog() {
-    this.dialog.open(ForgotMessageComponent, {
-      height: '200px',
-      width: '400px',
     });
   }
 }
