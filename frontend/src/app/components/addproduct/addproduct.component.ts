fb3230437d7a86ddc920a45482cc02743268379e
anchous <orlovska.anna@lll.kpi.ua>
2023-12-06 23:31:08 +0200

    mm

--- frontend/src/app/components/addproduct/addproduct.component.ts
+++ frontend/src/app/components/addproduct/addproduct.component.ts
@@ -36,6 +36,13 @@ export class AddproductComponent implements OnInit {
     supplier: new FormControl('', [Validators.required]),
     expiration_date: new FormControl('', [Validators.required]),
   });
+  currentDate(): string {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = (now.getMonth() + 1).toString().padStart(2, '0');
+    const day = now.getDate().toString().padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  }
 
   ngOnInit(): void {
   }
